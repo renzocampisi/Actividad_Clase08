74a6b7c141105731eb0ffa6296a5e44100de37e6
Renzo <campisirenzo0@gmail.com>
2022-05-24 17:08:52 -0300

    master: Resolucion de variables y op

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,26 @@
+// 1. Variables y Operadores
+    // a) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+    
+    /*     var texto = "";
+        var primerPalabra = "hola";
+        var segundaPalabra = "estas";
+
+        texto += primerPalabra;
+        texto += segundaPalabra;
+
+        var textoConcat = primerPalabra.concat("como", segundaPalabra);
+
+        console.log(textoConcat); */
+
+    // b) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+        /* var texto = "";
+        var primerPalabra = "hola";
+        var segundaPalabra = "estas";
+
+        texto += primerPalabra;
+        texto += segundaPalabra;
+
+        var textoConcat = primerPalabra.concat("como", segundaPalabra);
+
+        console.log(textoConcat.length); */
