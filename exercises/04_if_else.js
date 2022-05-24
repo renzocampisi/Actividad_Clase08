8ce643b60dca4105875981d6dc7b2ba0837fbcc2
Renzo <campisirenzo0@gmail.com>
2022-05-24 16:48:45 -0300

    master: Ejercicio if else resuelto

---
+++ exercises/04_if_else.js
@@ -0,0 +1,28 @@
+// 4. If Else
+    // a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”. [utilizar alert() ]
+        
+        /* var numero
+        numero = Math.random();
+        console.log(numero)
+
+        if (numero >= 0.5) {
+            console.log("Greater than 0,5")
+        } else {
+            console.log("Lower than 0,5")
+        } */
+
+    // b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+        
+        /* Age = Math.random() * 100;
+        Math.ceil(console.log(Age));
+
+        // “Menor” si la edad es menor a 18
+        if (Age < 18) {
+            console.log("Menor")
+        // “Mayor” si la edad es mayor o igual a 18 Y menor que 65
+        } else if (Age >= 18 && Age < 65) {
+            console.log("Mayor")
+        // “Jubilado” si la edad es mayor o igual que 65.     
+        } else if (Age >= 65) {
+            console.log("Jubilado")
+        }  */
