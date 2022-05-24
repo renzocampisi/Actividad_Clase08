92ad694a540c065898c5b521954e18b6794436ac
Renzo <campisirenzo0@gmail.com>
2022-05-24 17:28:08 -0300

    master: Resolucion ejercicio strings

---
+++ exercises/02_strings.js
@@ -0,0 +1,22 @@
+// 2. Strings
+    // a) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+        
+        /* const nombreCompleto = "renzo campisi";
+
+        const nombreNuevo = nombreCompleto;
+
+        console.log(nombreNuevo.slice(0,5)); */
+    
+    // b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+
+        /* const nombre1 = "fernando carreras";
+
+        const nombreMayus = nombre1;
+
+        console.log(nombreMayus.charAt(0).toUpperCase() + nombreMayus); */
+
+    // c) Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+
+        /* const espacio = "simon roberto";
+
+        console.log(espacio.indexOf(" ")); */
