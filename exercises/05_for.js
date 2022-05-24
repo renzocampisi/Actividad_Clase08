92e6365c869261de3bd1d843ce4964aa83c13170
Renzo <campisirenzo0@gmail.com>
2022-05-24 17:33:02 -0300

    master: Resolucion de ej. for

---
+++ exercises/05_for.js
@@ -0,0 +1,15 @@
+// 5. For
+    // a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+    
+        /* const array = ["teclado", "monitor", "mouse", "escritorio", "parlante"]
+        for (let i = 0; i < array.length; i++) {
+            console.log(array[i])
+        } */
+
+    // b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
+
+    // c) Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log).
+        
+        /* for (let i = 0; i < 10; i++) {
+            console.log(i)
+        } */
