f1c7ca77496acb07ff49776681cae34e26e8d77d
Renzo <campisirenzo0@gmail.com>
2022-05-24 17:38:33 -0300

    master: Resolucion de las funciones

---
+++ exercises/06_functions.js
@@ -0,0 +1,34 @@
+// 6. Funciones
+    // a) Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+        
+        /* function sumar(a, b){
+            return a + b
+        }
+        
+        const resultado = sumar(2, 3)
+        
+        console.log(resultado) */
+
+// b) A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
+
+        /* function sumar(a, b){
+            return a + b
+        }
+        const resultado = sumar(2, 3)
+        if (sumar == "") {
+            console.log("NaN")
+        }
+        else {
+            console.log(resultado) 
+        } */
+
+// c) A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
+    
+        /* const numero = 100;
+        function esEntero(numero){
+            if (numero - Math.floor(numero) == 0) {
+                console.log("Es un numero entero");
+            } else {
+                console.log("Es un numero decimal");
+            }
+        } */
